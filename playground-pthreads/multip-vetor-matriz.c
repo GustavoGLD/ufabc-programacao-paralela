cc05654efec9778ff27c69de49baaa3231153161
Gustavo Lidio Damaceno <72351688+GustavoGLD@users.noreply.github.com>
2025-07-31 17:18:39 +0000

    terminando pthreads multiplicacao de matriz X vetor

--- playground-pthreads/multip-vetor-matriz.c
+++ playground-pthreads/multip-vetor-matriz.c
@@ -1,22 +1,10 @@
-/*
-pseudo código sequencial
-for (i = 0; i < m; i++) {
-    y[i] = 0.0
-    
-    # para cada elemento da linhas e cada elemento de x
-    for (j = 0; j < n; j++) {
-        y[i] += A[i][j] * x[j];
-    }
-}
-*/
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 int thread_count;
 int len_linhas, len_cols;
-int *vetor_saida, *matriz_entrada, *vetor_entrada;
+double *vetor_saida, *matriz_entrada, *vetor_entrada;
 
 void *pth_mat_vector(void *rank) {
     long my_rank = (long) rank;
@@ -24,12 +12,12 @@ void *pth_mat_vector(void *rank) {
 
     int len_linhas_local = len_linhas / thread_count;
     int my_first_row = my_rank * len_linhas_local;
-    int my_last_row = (my_rank+1) * len_linhas_local - 1;
+    int my_last_row = (my_rank + 1) * len_linhas_local - 1;
 
-    for (i = my_first_row; i < my_last_row; i++) {
+    for (i = my_first_row; i <= my_last_row; i++) {
         vetor_saida[i] = 0.0;
         for (j = 0; j < len_cols; j++) {
-            vetor_saida[i] += matriz_entrada[i*len_cols+j] * vetor_entrada[j];
+            vetor_saida[i] += matriz_entrada[i * len_cols + j] * vetor_entrada[j];
         }
     }
 
@@ -49,20 +37,37 @@ int main(int argc, char* argv[]) {
     len_linhas = strtol(argv[2], NULL, 10);
     len_cols = strtol(argv[3], NULL, 10);
 
-    matriz_entrada = malloc(len_linhas * len_cols * sizeof(int));
-    vetor_entrada = malloc(len_cols * sizeof(int));
-    vetor_saida = malloc(len_linhas * sizeof(int));
+    matriz_entrada = malloc(len_linhas * len_cols * sizeof(double));
+    vetor_entrada = malloc(len_cols * sizeof(double));
+    vetor_saida = malloc(len_linhas * sizeof(double));
     thread_handles = malloc(thread_count * sizeof(pthread_t));
 
     for (int i = 0; i < len_linhas * len_cols; i++) {
-        matriz_entrada[i] = 1;
+        matriz_entrada[i] = 1.0;
     }
 
     for (int i = 0; i < len_cols; i++) {
-        vetor_entrada[i] = 1;
+        vetor_entrada[i] = 1.0;
     }
 
     for (thread = 0; thread < thread_count; thread++) {
-        // pthread_create(&)
+        pthread_create(&thread_handles[thread], NULL, pth_mat_vector, (void*) thread);
     }
+
+    for (thread = 0; thread < thread_count; thread++) {
+        pthread_join(thread_handles[thread], NULL);
+    }
+
+    printf("Vetor resultado:\n");
+    for (int i = 0; i < len_linhas; i++) {
+        printf("%lf ", vetor_saida[i]);
+    }
+    printf("\n");
+
+    free(matriz_entrada);
+    free(vetor_entrada);
+    free(vetor_saida);
+    free(thread_handles);
+
+    return 0;
 }
